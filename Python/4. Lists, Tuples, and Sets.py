0565f4099207456eb0f4f262c2e529be414bf720
LamersBart <92574075+LamersBart@users.noreply.github.com>
2021-11-10 15:51:56 +0100

    update

--- Python/4. Lists, Tuples, and Sets.py
+++ Python/4. Lists, Tuples, and Sets.py
@@ -37,4 +37,55 @@ popped = profielen.pop()
 print(popped)
 print(profielen)
 
-# 
+# Lijst omkeren
+profielen.reverse()
+print(profielen)
+
+# Lijst sorteren (Tekst op aflabet A-Z en nummers oplopend 1-9)
+nums = [2, 6, 3, 1, 5, 4]
+profielen.sort()
+nums.sort()
+print(profielen)
+print(nums)
+
+# Lijst sorteren en omdraaien (Tekst op aflabet Z-A en nummers aflopend 9-1)
+profielen.sort(reverse=True)
+nums.sort(reverse=True)
+print(profielen)
+print(nums)
+
+# functie sorted
+gesorteerde_profielen = sorted(profielen)
+print(gesorteerde_profielen)
+
+# functie minimale waarde, in dit voorbeeld 1
+print(min(nums))
+
+# funtie som, in dit voorbeeld 21
+print(sum(nums))
+
+# print het index nummer van een waarde in een list/array, in dit voorbeeld Infrastructuur
+print(profielen)
+print(profielen.index('Infrastructuur'))
+
+# True/False manrier om te controleren of zich iets in de lijst bevind
+print('Test' in profielen)  # False
+print('Media' in profielen) # True
+
+# For loop print lijst
+for item in profielen:
+    print(item)
+
+# For loop print lijst met numering startent met 1 ipv 0
+for index, item in enumerate(profielen, start=1):
+    print(index, item)
+
+# Join alle waardes in de lijst in 1 string met een komma als scheidingsteken
+profielen_str = ' - '.join(profielen)
+print(profielen_str)
+
+# Maak van 1 string met - gescheiden waardes een lijst.
+nieuwe_lijst = profielen_str.split(' - ')
+print(nieuwe_lijst)
+
+#
---
+++ Python/EigenProject.py
@@ -0,0 +1,31 @@
+# Eigen inbreng project
+# Een script die ervoor zorgt dat de locatie van verschillende agenten om de x secondes wordt upgedate.
+# Er moet een lijst zijn met verschillende waardes met LAT & LONG voor elk agent. (List)
+
+import requests
+
+latitudeList = []
+longitudeList = []
+allLocations = []
+
+def add_location(Latitude, Longitude):
+    location = {
+        "lat": Latitude,
+        "long": Longitude,
+    }
+    allLocations.append(location)
+
+add_location(51.463526, 5.474311)
+add_location(51.460959, 5.469501)
+add_location(51.456895, 5.470017)
+add_location(51.453526, 5.467256)
+add_location(51.449300, 5.470176)
+add_location(51.446197, 5.473955)
+add_location(51.447214, 5.479710)
+add_location(51.447802, 5.487869)
+add_location(51.449460, 5.498347)
+add_location(51.451814, 5.479655)
+
+for locations in allLocations:
+    print(locations)
+    r = requests.post("", params=locations)
